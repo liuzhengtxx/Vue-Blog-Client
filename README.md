414ee564e6850b4b8d31a912467d22a53a5a1619
evenyao <invictus@vip.qq.com>
2018-09-25 21:20:50 +0800

    add elementUI and api

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 ## 项目涉及到技术栈：
 - Vue：Vue、Vue-router、Vuex、Vue-cli
 - CSS的预处理框架：less
+- 插件：ElementUI、axios
+- 接口：auth.js、blog.js
+
+
+# 项目记录
+## 安装 / 导入 ElementUI
+```JavaScript
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI)
+```
--- src/pages/Index/Index.vue
+++ src/pages/Index/Index.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="index">
-    首页
+    <p>首页</p>
+    <div>
+      <el-button @click="onClick1">默认按钮</el-button>
+      <el-button @click="onClick2">alert</el-button>
+    </div>
   </div>
 </template>
 
 <script>
+import request from '@/helpers/request.js'
+import auth from '@/api/auth.js'
+import blog from '@/api/blog.js'
+window.request = request
+window.auth = auth
+window.blog = blog
+
 export default {
+  methods: {
+    onClick1 () {
+      this.$message.error('这是一条错误的信息。')
+    },
+
+    onClick2 () {
+      this.$alert('这是一段内容', '标题名称', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message.success('点了确定')
+        }
+      })
+    }
+  }
 }
 </script>
 
